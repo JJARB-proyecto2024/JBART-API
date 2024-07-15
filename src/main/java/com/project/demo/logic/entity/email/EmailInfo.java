3e751b4130f06e0f0f9fee7871a7954236a57e04
Burenam <burenam@ucenfotec.ac.cr>
2024-07-15 03:06:55 -0600

    feat: Send Email
    feat: Send Email

---
+++ src/main/java/com/project/demo/logic/entity/email/EmailInfo.java
@@ -0,0 +1,23 @@
+package com.project.demo.logic.entity.email;
+
+
+public class EmailInfo {
+    String name;
+    String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
