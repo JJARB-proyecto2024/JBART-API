1dc27ba4af0f2db5af28768b5b08548296555e6a
Andrey <andreyacostar@gmail.com>
2024-07-21 21:07:20 -0600

    fix

--- src/main/java/com/project/demo/rest/paypal/PaypalController.java
+++ src/main/java/com/project/demo/rest/paypal/PaypalController.java
@@ -6,33 +6,35 @@ import com.project.demo.logic.entity.paypal.PaypalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/paypal")
-@CrossOrigin(origins = "http://localhost:4200")
 public class PaypalController {
     @Autowired
     private PaypalService paypalService;
 
     @PostMapping("/createPayment")
-    public ResponseEntity<Payment> createPayment(
+    public ResponseEntity<String> createPayment(
             @RequestBody List<ItemDto> items,
-            @RequestHeader("host") String host)
-    {
+            @RequestHeader("host") String host) {
         try {
             String baseUrl = "http://" + host;
             Payment payment = paypalService.createPayment(items, baseUrl);
-            return  new ResponseEntity<>(payment, HttpStatus.CREATED);
-        }catch (PayPalRESTException e){
+            return new ResponseEntity<>(payment.toString(), HttpStatus.CREATED);
+        } catch (PayPalRESTException e) {
+            // Log error details and return a more informative message
             e.printStackTrace();
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error creating payment: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+
     @PostMapping("/executePayment")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Payment> executePayment(@RequestBody ExecutePaymentDto dto) {
         try {
             Payment payment = paypalService.excecutePayment(dto.getPaymentId(), dto.getPayerId());
