531ea2929cce598108d583548ec0eb07859959cc
Andrey <andreyacostar@gmail.com>
2024-07-25 15:58:43 -0600

    feat: add controller for sales and query

--- src/main/java/com/project/demo/logic/entity/order/OrderRepository.java
+++ src/main/java/com/project/demo/logic/entity/order/OrderRepository.java
@@ -2,8 +2,11 @@ package com.project.demo.logic.entity.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
@@ -12,5 +15,15 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT o FROM Order o WHERE o.userBuyer.id = ?1")
     List<Order> findByUserId(Long userId);
+
+    @Query("SELECT p.name AS productName, " +
+        "c.name AS categoryName, SUM(o.quantity) AS totalQuantitySold " +
+        "FROM Order o " +
+        "JOIN o.product p " +
+        "JOIN p.category c " +
+        "WHERE p.userBrand.id = :userBrandId " +
+        "GROUP BY p.Id, p.name, c.name " +
+        "ORDER BY totalQuantitySold DESC")
+    List<Object[]> getMostSoldProducts(@Param("userBrandId") Long userBrandId);
 
 }
