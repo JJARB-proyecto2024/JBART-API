01bb5640595fb3ed968e79fd7efb82c3aa8af185
Jaikel19 <jaikel.guzman@outlook.com>
2024-07-24 22:43:02 -0600

    feat: add order
    uncomment the entity, repository and seeder and change the method in the controller

--- src/main/java/com/project/demo/logic/entity/order/Order.java
+++ src/main/java/com/project/demo/logic/entity/order/Order.java
@@ -1,107 +1,107 @@
-//package com.project.demo.logic.entity.order;
-//
-//import com.project.demo.logic.entity.userBuyer.UserBuyer;
-//import com.project.demo.logic.entity.product.Product;
-//import jakarta.persistence.*;
-//import org.hibernate.annotations.CreationTimestamp;
-//import org.hibernate.annotations.UpdateTimestamp;
-//import java.util.Date;
-//
-//@Entity
-//@Table(name = "orders")
-//public class Order {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long Id;
-//    @ManyToOne
-//    @JoinColumn(name = "user_buyer_id")
-//    private UserBuyer userBuyer;
-//    @ManyToOne
-//    @JoinColumn(name = "product_id")
-//    private Product product;
-//    private Integer quantity;
-//    private Double subTotal;
-//    private Double shippingCost;
-//    private Double total;
-//    private String status;
-//
-//    @CreationTimestamp
-//    @Column(updatable = false, name = "created_at")
-//    private Date createdAt;
-//
-//    @UpdateTimestamp
-//    @Column(name = "updated_at")
-//    private Date updatedAt;
-//
-//    public Long getId() {
-//        return Id;
-//    }
-//
-//    public void setId(Long id) {
-//        Id = id;
-//    }
-//
-//    public UserBuyer getUserBuyer() {
-//        return userBuyer;
-//    }
-//
-//    public void setUserBuyer(UserBuyer userBuyer) {
-//        this.userBuyer = userBuyer;
-//    }
-//
-//    public Product getProduct() {
-//        return product;
-//    }
-//
-//    public void setProduct(Product product) {
-//        this.product = product;
-//    }
-//
-//    public Integer getQuantity() {
-//        return quantity;
-//    }
-//
-//    public void setQuantity(Integer quantity) {
-//        this.quantity = quantity;
-//    }
-//
-//    public Double getSubTotal() {
-//        return subTotal;
-//    }
-//
-//    public void setSubTotal(Double subTotal) {
-//        this.subTotal = subTotal;
-//    }
-//
-//    public Double getShippingCost() {
-//        return shippingCost;
-//    }
-//
-//    public void setShippingCost(Double shippingCost) {
-//        this.shippingCost = shippingCost;
-//    }
-//
-//    public Double getTotal() {
-//        return total;
-//    }
-//
-//    public void setTotal(Double total) {
-//        this.total = total;
-//    }
-//
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public Date getCreatedAt() {return createdAt;}
-//
-//    public void setCreatedAt(Date createdAt) {this.createdAt = createdAt;}
-//
-//    public Date getUpdatedAt() {return updatedAt;}
-//
-//    public void setUpdatedAt(Date updatedAt) {this.updatedAt = updatedAt;}
-//}
+package com.project.demo.logic.entity.order;
+
+import com.project.demo.logic.entity.userBuyer.UserBuyer;
+import com.project.demo.logic.entity.product.Product;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @ManyToOne
+    @JoinColumn(name = "user_buyer_id")
+    private UserBuyer userBuyer;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Integer quantity;
+    private Double subTotal;
+    private Double shippingCost;
+    private Double total;
+    private String status;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public UserBuyer getUserBuyer() {
+        return userBuyer;
+    }
+
+    public void setUserBuyer(UserBuyer userBuyer) {
+        this.userBuyer = userBuyer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(Double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public Double getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(Double shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {return createdAt;}
+
+    public void setCreatedAt(Date createdAt) {this.createdAt = createdAt;}
+
+    public Date getUpdatedAt() {return updatedAt;}
+
+    public void setUpdatedAt(Date updatedAt) {this.updatedAt = updatedAt;}
+}
