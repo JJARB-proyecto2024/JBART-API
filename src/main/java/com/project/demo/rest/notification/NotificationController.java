49752dbd5f21855fd862670d762024eb947059f7
Maxwell Chinchilla Abarca <64172254+maxwell-abarca@users.noreply.github.com>
2024-08-09 00:57:58 -0600

    chore: removed socket configuration

--- src/main/java/com/project/demo/rest/notification/NotificationController.java
+++ src/main/java/com/project/demo/rest/notification/NotificationController.java
@@ -4,7 +4,6 @@ import com.project.demo.logic.entity.notification.Notification;
 import com.project.demo.logic.entity.notification.NotificationRepository;
 import com.project.demo.logic.entity.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +15,6 @@ public class NotificationController {
 
     @Autowired
     private NotificationRepository notificationRepository;
-
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
 
     @GetMapping
     @PreAuthorize("isAuthenticated()")
@@ -34,9 +30,7 @@ public class NotificationController {
     @PostMapping
     @PreAuthorize("isAuthenticated()")
     public Notification addNotification(@RequestBody Notification notification) {
-        Notification savedNotification = notificationRepository.save(notification);
-        messagingTemplate.convertAndSend("/topic/notifications", savedNotification);
-        return savedNotification;
+        return notificationRepository.save(notification);
     }
 
     @GetMapping("/{id}")
