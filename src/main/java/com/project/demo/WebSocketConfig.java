4406d98f9db79cb6d44a479539a24b1c43bd7c73
Maxwell Chinchilla Abarca <64172254+maxwell-abarca@users.noreply.github.com>
2024-08-12 19:06:00 -0600

    reverted to a previous commit Revert "chore: added websocket filtering by userid"
    This reverts commit 6dc4bf2bc6c338f377c98be980a9d042d36690cc.

--- src/main/java/com/project/demo/WebSocketConfig.java
+++ src/main/java/com/project/demo/WebSocketConfig.java
@@ -1,18 +1,11 @@
 package com.project.demo;
 
 import com.project.demo.logic.entity.notification.NotificationHandler;
-import com.project.demo.logic.entity.user.User;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSocket
@@ -27,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(notificationHandler, "/ws/notifications")
-                .setAllowedOrigins("*") // Permitir todas las conexiones de origen
+                .setAllowedOrigins("*")
                 .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 }
