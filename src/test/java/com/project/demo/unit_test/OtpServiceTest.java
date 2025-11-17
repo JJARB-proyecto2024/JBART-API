556fae140d277d070ce631754baf2501b8448f8d
Andrey Acosta <andreyacostar@gmail.com>
2025-11-17 12:39:13 -0600

    fix(class test)

--- src/test/java/com/project/demo/unit_test/OtpServiceTest.java
+++ src/test/java/com/project/demo/unit_test/OtpServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class OtpServiceTest {
+class OtpServiceTest {
 
     @Mock
     private OtpRepository otpRepository;
