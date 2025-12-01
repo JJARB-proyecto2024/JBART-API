1e51f71021e4270173effb0a6ed3761edfcff275
Andrey Acosta <andreyacostar@gmail.com>
2025-12-01 12:03:50 -0600

    feat(CI)

--- src/test/java/com/project/demo/unit_test/ProductTest.java
+++ src/test/java/com/project/demo/unit_test/ProductTest.java
@@ -1,8 +1,68 @@
 package com.project.demo.unit_test;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import com.project.demo.logic.entity.category.Category;
+import com.project.demo.logic.entity.product.Product;
+import com.project.demo.logic.entity.userBrand.UserBrand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ProductTest {
+
+    private Product product;
+
+
+    @BeforeEach
+    void setUp() {
+        product = new Product();
+    }
+
+    @Test
+    void testGettersAndSetters() {
+        Long id = 1L;
+        String name = "Coffee Premium";
+        Double price = 10.99;
+        String size = "Small";
+        Integer quantity = 1500;
+        String status = "Active";
+        Integer rate = 5;
+        String model = "Bag 500g";
+
+        product.setId(id);
+        product.setName(name);
+        product.setPrice(price);
+        product.setSize(size);
+        product.setQuantityInStock(quantity);
+        product.setStatus(status);
+        product.setRate(rate);
+        product.setModel(model);
+
+        assertEquals(id, product.getId());
+        assertEquals(name, product.getName());
+        assertEquals(price, product.getPrice());
+        assertEquals(size, product.getSize());
+        assertEquals(quantity, product.getQuantityInStock());
+        assertEquals(status, product.getStatus());
+        assertEquals(rate, product.getRate());
+        assertEquals(model, product.getModel());
+    }
+
+    @Test
+    void testRelations_Between_Objets() {
+        Category category = new Category();
+        category.setName("Drinks");
+
+        product.setCategory(category);
+        assertNotNull(product.getCategory());
+        assertEquals("Drinks", product.getCategory().getName());
+
+        UserBrand userBrand = new UserBrand();
+        userBrand.setName("Starbucks");
+
+        product.setUserBrand(userBrand);
+        assertNotNull(product.getUserBrand());
+        assertEquals("Starbucks", product.getUserBrand().getName());
+    }
 }
--- src/test/java/com/project/demo/unit_test/UserTest.java
+++ src/test/java/com/project/demo/unit_test/UserTest.java
@@ -1,27 +1,77 @@
 package com.project.demo.unit_test;
 
-import com.project.demo.logic.entity.user.UserRepository;
+import com.project.demo.logic.entity.enums.RoleEnum;
+import com.project.demo.logic.entity.enums.StatusEnum;
+import com.project.demo.logic.entity.rol.Role;
+import com.project.demo.logic.entity.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.util.function.BooleanSupplier;
+import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
-@EntityScan("com.project.demo.logic.entity.user")
-@EnableJpaRepositories("com.project.demo.logic.entity.user")
+
 class UserTest {
-    @Autowired
-    private UserRepository userRepository;
+
+    private User user;
+
+    private static final String TEST_EMAIL = "test@gmail.com";
+    private static final String TEST_PASSWORD = "password";
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @Test
+    void testGettersAndSetters() {
+        Long id = 1L;
+        String email = TEST_EMAIL;
+        String password = TEST_PASSWORD;
+        String name = "Andrey";
+
+        user.setId(id);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName(name);
+        user.setStatus(StatusEnum.ACTIVE);
+
+
+        assertEquals(id, user.getId());
+        assertEquals(email, user.getEmail());
+        assertEquals(password, user.getPassword());
+        assertEquals(StatusEnum.ACTIVE, user.getStatus());
+    }
 
 
     @Test
-    void findUserByName() {
-        assertTrue((BooleanSupplier) userRepository.findUserByName("Ashely"));
+    void testUserDetailsMethods() {
+        user.setEmail(TEST_EMAIL);
+
+        assertEquals(TEST_EMAIL, user.getEmail());
+
+        assertTrue(user.isAccountNonExpired());
+        assertTrue(user.isAccountNonLocked());
+        assertTrue(user.isCredentialsNonExpired());
+        assertTrue(user.isEnabled());
+    }
+
+
+    @Test
+    void testAuthorities() {
+        Role role = new Role();
+        role.setName(RoleEnum.USER);
+
+        user.setRole(role);
+
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        assertNotNull(authorities);
+        assertEquals(1, authorities.size());
+
+        String authorityName = authorities.iterator().next().getAuthority();
+        assertEquals("ROLE_USER", authorityName);
     }
 
 }
